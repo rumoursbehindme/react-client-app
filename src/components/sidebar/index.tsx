d2f25ddeb55c049de520ab87d1a7364cc87b2a89
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-10-11 18:46:45 +0530

    feat: added active menu feature to side bar buttons

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -11,13 +11,13 @@ export default function SideBar() {
         <div className='sidebar-container'>
             <img src='logo192.png' className='profile-img' alt='profile' />
             <div className='navigation-container'>
-                <SidebarButton title='Feed' to='/feed' icon={<MdSpaceDashboard />} />
-                <SidebarButton title='Trending' to='/trending' icon={<FaGripfire />} />
-                <SidebarButton title='Player' to='/player' icon={<FaPlay />} />
-                <SidebarButton title='Favorites' to='/favorites' icon={<MdFavorite />} />
-                <SidebarButton title='Library' to='/' icon={<IoLibrary />} />
+                <SidebarButton title='Feed' redirectUrl='/feed' icon={<MdSpaceDashboard />} />
+                <SidebarButton title='Trending' redirectUrl='/trending' icon={<FaGripfire />} />
+                <SidebarButton title='Player' redirectUrl='/player' icon={<FaPlay />} />
+                <SidebarButton title='Favorites' redirectUrl='/favorites' icon={<MdFavorite />} />
+                <SidebarButton title='Library' redirectUrl='/' icon={<IoLibrary />} />
             </div>
-            <SidebarButton title='Sign Out' to='/signout' icon={<FaSignOutAlt />} />
+            <SidebarButton title='Sign Out' redirectUrl='/signout' icon={<FaSignOutAlt />} />
         </div>
     )
 }
