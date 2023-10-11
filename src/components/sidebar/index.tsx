d97c3303cd9c111c152c579713daab44ffed0a7b
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-10-11 16:05:50 +0530

    feat: added side bar buttton in side bar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import './sidebar.css'
+import './styles/sidebar.css'
+import SidebarButton from './sidebar-button'
 
 export default function SideBar() {
-  return (
-    <div className='sidebar-container'>SideBar</div>
-  )
+    return (
+        <div className='sidebar-container'>
+            <img src='logo192.png' className='profile-img' alt='profile' />
+            <div>
+                <SidebarButton />
+                <SidebarButton />
+                <SidebarButton />
+                <SidebarButton />
+                <SidebarButton />
+            </div>
+            <SidebarButton />
+        </div>
+    )
 }
