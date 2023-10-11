7a21a9d2b6bd698d7dc1ab5328f3a8a254e3b05e
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-10-11 18:18:55 +0530

    feat: added reac-icons and css to sidebar menu

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import './styles/sidebar.css'
-import SidebarButton from './sidebar-button'
+import React from 'react';
+import './styles/sidebar.css';
+import SidebarButton from './sidebar-button';
+import { MdSpaceDashboard, MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay, FaSignOutAlt } from 'react-icons/fa';
+import { IoLibrary } from "react-icons/io5";
+
 
 export default function SideBar() {
     return (
         <div className='sidebar-container'>
             <img src='logo192.png' className='profile-img' alt='profile' />
-            <div>
-                <SidebarButton />
-                <SidebarButton />
-                <SidebarButton />
-                <SidebarButton />
-                <SidebarButton />
+            <div className='navigation-container'>
+                <SidebarButton title='Feed' to='/feed' icon={<MdSpaceDashboard />} />
+                <SidebarButton title='Trending' to='/trending' icon={<FaGripfire />} />
+                <SidebarButton title='Player' to='/player' icon={<FaPlay />} />
+                <SidebarButton title='Favorites' to='/favorites' icon={<MdFavorite />} />
+                <SidebarButton title='Library' to='/' icon={<IoLibrary />} />
             </div>
-            <SidebarButton />
+            <SidebarButton title='Sign Out' to='/signout' icon={<FaSignOutAlt />} />
         </div>
     )
 }
--- src/components/sidebar/sidebar-button.tsx
+++ src/components/sidebar/sidebar-button.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { ISidebarButtonProperties } from './types'
+import { IconContext } from 'react-icons';
+import { Link } from 'react-router-dom'
+import './styles/sidebar-button.css'
 
-export default function SidebarButton() {
+export default function SidebarButton(properties: ISidebarButtonProperties) {
+  const { to, title, icon } = properties;
   return (
-    <div>SidebarButton</div>
+    <Link to={to}>
+      <div className='sidebar-button-body'>
+        <IconContext.Provider value={{ size: '24px', className: "sidebar-button-sub-container" }}>
+          {icon}
+          <p className='sidebar-button-title'>{title}</p>
+        </IconContext.Provider>
+      </div>
+    </Link>
   )
 }
