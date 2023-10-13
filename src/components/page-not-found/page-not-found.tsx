27a8d9b13a68816735843a7730d378eb8185d9a6
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-10-13 16:09:20 +0530

    fix: added relative path to image src

--- src/components/page-not-found/page-not-found.tsx
+++ src/components/page-not-found/page-not-found.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './page-not-found.css';
-import pageNotFound from '../../../public/assets/png/page-not-found.png';
 
 export default function PageNotFound() {
   return (
     <div className='screen-container' >
-      <h1>Page Not Found !</h1>
-      <img className='page-not-found-image' src={pageNotFound} alt="logo" />
+        <h1>Page Not Found !</h1>
+      <img className='page-not-found-image' src='/assets/png/page-not-found.png' alt="logo" />
     </div>
   )
 }
