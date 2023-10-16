42af87c581073d089c6afc96d6046641354087bc
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-10-16 19:14:50 +0530

    feat: integrated localization to side bar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,14 +4,16 @@ import SidebarButton from './sidebar-button';
 import { MdSpaceDashboard, MdFavorite } from "react-icons/md";
 import { FaGripfire, FaPlay, FaSignOutAlt } from 'react-icons/fa';
 import { IoLibrary } from "react-icons/io5";
-
+import { getTranslater } from '../../common/helper/localization-helper';
 
 export default function SideBar() {
+
+    const t = getTranslater('sidebar')
     return (
         <div className='sidebar-container'>
             <img src='logo192.png' className='profile-img' alt='profile' />
             <div className='navigation-container'>
-                <SidebarButton title='Feed' redirectUrl='/feed' icon={<MdSpaceDashboard />} />
+                <SidebarButton title={t('feed')} redirectUrl='/feed' icon={<MdSpaceDashboard />} />
                 <SidebarButton title='Trending' redirectUrl='/trending' icon={<FaGripfire />} />
                 <SidebarButton title='Player' redirectUrl='/player' icon={<FaPlay />} />
                 <SidebarButton title='Favorites' redirectUrl='/favorites' icon={<MdFavorite />} />
