9e1a5167befb832932040b0d4fcf042357cf624b
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-10-16 19:13:37 +0530

    feat: added translation helper method

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//required to enable localization throughout the application
+import './localization/localization';  
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
