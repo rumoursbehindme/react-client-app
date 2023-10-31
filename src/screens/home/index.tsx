9e7714f1061a8219c876df60a2190c4259edf560
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-10-31 14:14:28 +0530

    feat: implemented spotify

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Library from '../library';
 import Feed from '../feed';
@@ -8,22 +8,48 @@ import Trending from '../trending';
 import './home.css';
 import SideBar from '../../components/sidebar';
 import PageNotFound from '../../components/page-not-found/page-not-found';
+import Login from '../auth/login';
+import { setClientToken } from '../../spotify';
 
 function Home() {
+
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        const token = window.localStorage.getItem('token');
+        if (token && token !== 'undefined') {
+            setToken(token)
+            setClientToken(token);
+        } else {
+            const hash = window.location.hash;
+            const accessToken = hash.split('&')[0].split('=')[1];
+
+            const localStorageValue = {
+                value: accessToken,
+                expiration: new Date().getTime() + 30 * 1000
+            }
+            window.localStorage.setItem('token', JSON.stringify(localStorageValue));
+            setToken(accessToken);
+            setClientToken(accessToken);
+        }
+    }, [])
+
     return (
-        <Router>
-            <div className='main-container'>
-                <SideBar />
-                <Routes>
-                    <Route path='/' element={<Library />} />
-                    <Route path='/feed' element={<Feed />} />
-                    <Route path='/player' element={<Player />} />
-                    <Route path='/favorites' element={<Favorites />} />
-                    <Route path='/trending' element={<Trending />} />
-                    <Route path='*' element={<PageNotFound />} />
-                </Routes>
-            </div>
-        </Router >
+        !token ?
+            <Login /> :
+            <Router>
+                <div className='main-container'>
+                    <SideBar />
+                    <Routes>
+                        <Route path='/' element={<Library />} />
+                        <Route path='/feed' element={<Feed />} />
+                        <Route path='/player' element={<Player />} />
+                        <Route path='/favorites' element={<Favorites />} />
+                        <Route path='/trending' element={<Trending />} />
+                        <Route path='*' element={<PageNotFound />} />
+                    </Routes>
+                </div>
+            </Router >
     )
 }
 
--- src/screens/library/index.tsx
+++ src/screens/library/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { apiClient } from '../../spotify'
+
 
 export default function Library() {
+
+  const [playlists, setPlaylists] = useState<any[]>([]);
+
+  useEffect(() => {
+    apiClient.get('me/playlists').then(function (response) {
+      console.log(response.data.items);
+      setPlaylists(response.data.items)
+    })
+  },[])
+
   return (
-    <div className='screen-container'>Library</div>
+    <div className='screen-container'>
+      {
+        playlists?.map(playlist=>
+          <div key={playlist.id}>{playlist.name}</div>)
+      }
+    </div>
   )
 }
