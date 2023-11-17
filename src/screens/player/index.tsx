60aeefcc6309db95829a0fd56c9838ad60c1c3c2
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-11-17 19:19:48 +0530

    feat: added playlist fethcing

--- src/screens/player/index.tsx
+++ src/screens/player/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './player.css';
+import { useLocation } from 'react-router-dom';
+import { IPlayListItemsProperty, IPlayListProperty, ITrackProperties } from './types';
 
 export default function Player() {
+  const [items, setItems] = useState<IPlayListItemsProperty[] | null>(null);
+  const [currentTrack, setCurrentTrack] = useState<ITrackProperties | null>(null);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state.id) {
+      fetch(`/playlists/${location.state.id}`)
+        .then(response => response.json())
+        .then((data: IPlayListProperty) => {
+          setItems(data.items);
+          setCurrentTrack(data.items[0].track)
+        })
+    }
+  }, [location.state.id]);
+
   return (
-    <div className='screen-container'>Player</div>
+    <div className='screen-container'></div>
   )
 }
