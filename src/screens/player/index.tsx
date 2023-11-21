fbefb94ef557c122302969c2b80bbfb124f94bc9
Rajeev Hegde <rajeev.hegde@agilepoint.com>
2023-11-21 18:45:46 +0530

    feat: added song card and upnext component

--- src/screens/player/index.tsx
+++ src/screens/player/index.tsx
@@ -1,26 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './player.css';
 import { useLocation } from 'react-router-dom';
-import { IPlayListItemsProperty, IPlayListProperty, ITrackProperties } from './types';
+import { IPlayListItemsProperty, IPlayListProperty, ITrackProperties } from '../../types';
+import SongCard from '../../components/song-card/song-card';
+import UpNext from '../../components/upnext/upnext';
 
 export default function Player() {
-  const [items, setItems] = useState<IPlayListItemsProperty[] | null>(null);
-  const [currentTrack, setCurrentTrack] = useState<ITrackProperties | null>(null);
+  const [items, setItems] = useState<IPlayListItemsProperty[]>();
+  const [currentTrack, setCurrentTrack] = useState<ITrackProperties>();
+  const [upNextIndex, setUpNextIndex] = useState(-1);
+  const [clickedSong, setClickedSong] = useState<ITrackProperties | undefined>();
 
   const location = useLocation();
 
   useEffect(() => {
-    if (location.state.id) {
-      fetch(`/playlists/${location.state.id}`)
+    if (upNextIndex !== -1 && items) {
+      setClickedSong(items[upNextIndex].track);
+    }
+  }, [upNextIndex, items]);
+
+  useEffect(() => {
+    if (location.state?.id) {
+      fetch(`/api/playlists/${location.state.id}`)
         .then(response => response.json())
         .then((data: IPlayListProperty) => {
           setItems(data.items);
-          setCurrentTrack(data.items[0].track)
-        })
+          setCurrentTrack(data.items[0].track);
+        });
+    } else {
+      try {
+        console.log('fetching playlists');
+        fetch('/api/playlists')
+          .then(response => response.json())
+          .then(data => {
+            const randomIndex = Math.floor(Math.random() * data.items.length);
+            console.log(`fetching this song ${data.items[randomIndex].track}`);
+            fetch(`/api/playlists/${data?.items[randomIndex].id}`)
+              .then(response => response.json())
+              .then((data: IPlayListProperty) => {
+                setItems(data?.items);
+                setCurrentTrack(data?.items[0]?.track);
+              });
+          });
+      } catch (e) {
+        console.log(e);
+      }
     }
-  }, [location.state.id]);
+  }, [location.state]);
 
   return (
-    <div className='screen-container'></div>
-  )
+    <div className='screen-container'>
+      <div className='player-container'>
+        <SongCard track={clickedSong || currentTrack!} />
+        <UpNext items={items!} setUpNextIndex={setUpNextIndex} sliceIndex={upNextIndex} />
+      </div>
+    </div>
+  );
 }
--- src/screens/playlists/index.tsx
+++ src/screens/playlists/index.tsx
@@ -43,7 +43,7 @@ const Playlists = () => {
   const [playlists, setPlaylists] = useState<IItemProperties[]>([]);
 
   useEffect(() => {
-    fetch('/playlists')
+    fetch('/api/playlists')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
